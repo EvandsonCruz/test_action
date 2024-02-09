cc0458840ebf57289bc1a7461e20be874fc4fa6e
Evandson Cruz <37565222+EvandsonCruz@users.noreply.github.com>
2024-02-09 20:31:36 -0300

    Update teste61.sql

--- teste61.sql
+++ teste61.sql
@@ -1,4 +1,4 @@
-create table ab.TESTETESTETESTETESTETESTETESTE (col1 number);       
+create table ab.TESTETESTETESTETESTETESTETESTEt (col1 number);       
 /
 grant select on ab.TESTETESTETESTETESTETESTETESTEt to user;
 /
