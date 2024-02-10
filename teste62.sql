2643cd9c284ebbc63e0dca80a01d0adb00addbfd
Evandson Cruz <37565222+EvandsonCruz@users.noreply.github.com>
2024-02-09 23:36:45 -0300

    Update teste62.sql

--- teste62.sql
+++ teste62.sql
@@ -1,4 +1,4 @@
-create table ab.testetestetestetestetesteteste_tttt (col1 number)
+create table ab.testetestetestetestetesteteste (col1 number)
 /
-grant select on ab.testetestetestetestetesteteste_tttt
+grant select on ab.testetestetestetestetesteteste
 /
