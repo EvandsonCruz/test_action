b00dbba532e5151873ddb36ea9c5f65e6ebc1045
Evandson Cruz <37565222+EvandsonCruz@users.noreply.github.com>
2024-02-20 00:37:59 -0300

    Update teste.sql

--- teste.sql
+++ teste.sql
@@ -1,14 +1,15 @@
 WITH RankedJobs AS (
     SELECT 
         job_name,
-        elapsed_time,
-        ROW_NUMBER() OVER (PARTITION BY job_name ORDER BY start_date DESC) AS rn
+        run_duration,
+        actual_start_date,
+        ROW_NUMBER() OVER (PARTITION BY job_name ORDER BY actual_start_date DESC) AS rn
     FROM 
-        dba_scheduler_run_job_details
+        dba_scheduler_job_run_details
 )
 SELECT
     job_name,
-    AVG(elapsed_time) AS avg_elapsed_time
+    AVG(run_duration) AS avg_run_duration
 FROM
     RankedJobs
 WHERE
