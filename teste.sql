b387c4e5d2b70bc20a7c458f31d3b9be4a0c110a
Evandson Cruz <37565222+EvandsonCruz@users.noreply.github.com>
2024-02-20 00:46:48 -0300

    Update teste.sql

--- teste.sql
+++ teste.sql
@@ -1,17 +1,19 @@
-WITH RankedJobs AS (
+WITH RunningJobs AS (
     SELECT 
         job_name,
-        run_duration,
+        EXTRACT(SECOND FROM run_duration) AS run_duration_seconds,
         actual_start_date,
         ROW_NUMBER() OVER (PARTITION BY job_name ORDER BY actual_start_date DESC) AS rn
     FROM 
         dba_scheduler_job_run_details
+    WHERE
+        end_date IS NULL  -- Adicione esta condição para incluir apenas jobs em execução
 )
 SELECT
     job_name,
-    AVG(run_duration) AS avg_run_duration
+    AVG(run_duration_seconds) AS avg_run_duration_seconds
 FROM
-    RankedJobs
+    RunningJobs
 WHERE
     rn <= 3
 GROUP BY
