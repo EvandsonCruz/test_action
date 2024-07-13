ca270e662e11793f71b5ed93e6bac48e906f1c1f
Evandson Cruz <37565222+EvandsonCruz@users.noreply.github.com>
2024-07-13 18:51:48 -0300

    Update table.sql

--- teste/table.sql
+++ teste/table.sql
@@ -2,5 +2,5 @@ create table ab.abc (
 dt_atlz varchar2(30),
 DT_CRIACAO varchar2(30),
 USER_CRIACAO varchar2(30),
-USUARIO_ATUALIZACAO varchar2(30)
+USUARIO_ATUALIZACAO varchar2(30) 
 )             
