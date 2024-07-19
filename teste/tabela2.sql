ec0e0bf143b0bf135a4085840e48e3a3c03c7ed4
Evandson Cruz <37565222+EvandsonCruz@users.noreply.github.com>
2024-07-19 14:20:27 -0300

    Update tabela2.sql

--- teste/tabela2.sql
+++ teste/tabela2.sql
@@ -40,19 +40,16 @@ validar-tablespace:
         else
           echo "PR OK"
         fi
-		
-  validar-colunas:
-    runs-on: deploy
 
-    steps:
+validar-colunas:
+  runs-on: deploy
+  steps:
     - name: Checkout do repositório
       uses: actions/checkout@v2
       with:
         fetch-depth: 0
-
     - name: Configurar variável de ambiente GH_TOKEN
       run: echo "GH_TOKEN=${{ secrets.DEVOPS_TOKEN }}" >> $GITHUB_ENV 
-
     - name: Obter lista de arquivos alterados
       run: |
         problemas=false
@@ -91,24 +88,21 @@ validar-tablespace:
           
           gh pr comment $PR_NUMBER -R $REPO_OWNER/$REPO_NAME --body "@$AUTHOR_NAME
           A criação da tabela nos seguintes arquivos não tem todos os campos de auditoria necessários:
-        $mensagem_erro"
-        exit 1
+          ${mensagem_erro}"
+          exit 1
         else
-          echo "PR OK"
+          echo "Todas as tabelas têm os campos de auditoria necessários."
         fi
-		
-  validar-encoding:
-    runs-on: deploy
 
-    steps:
+validar-encoding:
+  runs-on: deploy
+  steps:
     - name: Checkout do repositório
       uses: actions/checkout@v2
       with:
         fetch-depth: 0
-
     - name: Configurar variável de ambiente GH_TOKEN
       run: echo "GH_TOKEN=${{ secrets.DEVOPS_TOKEN }}" >> $GITHUB_ENV
-
     - name: Obter lista de arquivos alterados
       run: |
         arquivos_alterados=($(git diff --name-only HEAD^1))
@@ -131,96 +125,92 @@ validar-tablespace:
           REPO_NAME=$(echo "${{ github.repository }}" | cut -d '/' -f 2)
           AUTHOR_NAME=${{ github.event.pull_request.user.login }}
           gh pr comment $PR_NUMBER -R $REPO_OWNER/$REPO_NAME --body "@$AUTHOR_NAME
-         Existe arquivo que não está codificado como UTF-8.
+          Existe arquivo que não está codificado como UTF-8.
           $saida Por favor, corrija o encoding e faça um novo commit."
           exit 1
+        else
+          echo "PR OK"
         fi
-		
-  validar-grant:
-    runs-on: deploy
 
-    steps:
-      - name: Checkout do código
-        uses: actions/checkout@v2
-        with:
-          fetch-depth: 0
+validar-grant:
+  runs-on: deploy
+  steps:
+    - name: Checkout do código
+      uses: actions/checkout@v2
+      with:
+        fetch-depth: 0
+    - name: Configurar variável de ambiente GH_TOKEN
+      run: echo "GH_TOKEN=${{ secrets.GH_TOKEN }}" >> $GITHUB_ENV 
+    - name: Imprimir alterações
+      run: |
+        echo "Referência antes do evento: ${{ github.event.before }}"
+        echo "SHA do commit atual: ${{ github.sha }}"
+        git diff --name-only ${{ github.event.before }} ${{ github.sha }}  
+    - name: Verificar Conteúdo do PR
+      run: |
+        palavras_chave=("grant")  # Substitua pela lista de palavras-chave desejada
+        outras_palavras=("to public")  # Substitua pela segunda lista de palavras 
+        encontrada=false 
+        saida=""
+        echo ">>>>>>>>>antes do for>>>>>>>>>>> $encontrada"
+        echo "#############################################################  ${{ github.event_name }}"
+        echo "##########################opened###################################  ${{ github.event.action }}"
 
-      - name: Configurar variável de ambiente GH_TOKEN
-        run: echo "GH_TOKEN=${{ secrets.GH_TOKEN }}" >> $GITHUB_ENV 
+        if [[ "${{ github.event.action }}" == "opened" || "${{ github.event.action }}" == "reopened"  ]]; then
+          echo "Pull request acabou de ser aberto. Verificando novos arquivos adicionados."
 
-      - name: Imprimir alterações
-        run: |
-          echo "Referência antes do evento: ${{ github.event.before }}"
-          echo "SHA do commit atual: ${{ github.sha }}"
-          git diff --name-only ${{ github.event.before }} ${{ github.sha }}  
+          # Obtém a lista de arquivos adicionados no pull request
+          arquivos_adicionados=($(git diff --name-only HEAD^1))
+          echo "##########################arquivos_adicionados###################################  $arquivos_adicionados"
 
-      - name: Verificar Conteúdo do PR
-        run: |
-          palavras_chave=("grant")  # Substitua pela lista de palavras-chave desejada
-          outras_palavras=("to public")  # Substitua pela segunda lista de palavras 
+          for arquivo in "${arquivos_adicionados[@]}"; do
+            echo "Novo arquivo adicionado: $arquivo"
 
-          encontrada=false 
-          saida=""
-
-          echo ">>>>>>>>>antes do for>>>>>>>>>>> $encontrada"
-          echo "#############################################################  ${{ github.event_name }}"
-          echo "##########################opened###################################  ${{ github.event.action }}"
-          
-           if [[ "${{ github.event.action }}" == "opened" || "${{ github.event.action }}" == "reopened"  ]]; then
-            echo "Pull request acabou de ser aberto. Verificando novos arquivos adicionados."
-
-            # Obtém a lista de arquivos adicionados no pull request
-            arquivos_adicionados=($(git diff --name-only HEAD^1))
-            echo "##########################arquivos_adicionados###################################  $arquivos_adicionados"
-
-            for arquivo in "${arquivos_adicionados[@]}"; do
-              echo "Novo arquivo adicionado: $arquivo"
-
-              # Restante do seu código para verificar palavras-chave e outras_palavras
-              for palavra_chave in "${palavras_chave[@]}"; do
-                if grep -q -i "$palavra_chave" "$arquivo"; then
-                  echo "Encontrada a palavra '$palavra_chave' no novo arquivo: $arquivo"
-                  for outra_palavra in "${outras_palavras[@]}"; do
-                    if grep -q -i "$outra_palavra" "$arquivo"; then
-                      echo "Encontrada a palavra '$outra_palavra' no mesmo arquivo: $arquivo"
-                      encontrada=true
-                      saida="$saida$palavra_chave $outra_palavra no arquivo $arquivo\n"
-                    fi
-                  done
-                fi
-              done
+            # Restante do seu código para verificar palavras-chave e outras_palavras
+            for palavra_chave in "${palavras_chave[@]}"; do
+              if grep -q -i "$palavra_chave" "$arquivo"; then
+                echo "Encontrada a palavra '$palavra_chave' no novo arquivo: $arquivo"
+                for outra_palavra in "${outras_palavras[@]}"; do
+                  if grep -q -i "$outra_palavra" "$arquivo"; then
+                    echo "Encontrada a palavra '$outra_palavra' no mesmo arquivo: $arquivo"
+                    encontrada=true
+                    saida="$saida$palavra_chave $outra_palavra no arquivo $arquivo\n"
+                  fi
+                done
+              fi
             done
-          else 
-            for arquivo in $(git diff --name-only ${{ github.event.before }} ${{ github.sha }}); do
-              for palavra_chave in "${palavras_chave[@]}"; do
-                if grep -q -i "$palavra_chave" "$arquivo"; then
-                  echo "Encontrada a palavra '$palavra_chave' no arquivo: $arquivo"
-                  for outra_palavra in "${outras_palavras[@]}"; do
-                    if grep -q -i "$outra_palavra" "$arquivo"; then
-                      echo "Encontrada a palavra '$outra_palavra' no mesmo arquivo: $arquivo"
-                      encontrada=true
-                      saida="$saida$palavra_chave $outra_palavra no arquivo $arquivo\n"
-                    fi
-                  done
-                fi
-              done
+          done
+        else 
+          for arquivo in $(git diff --name-only ${{ github.event.before }} ${{ github.sha }}); do
+            for palavra_chave in "${palavras_chave[@]}"; do
+              if grep -q -i "$palavra_chave" "$arquivo"; then
+                echo "Encontrada a palavra '$palavra_chave' no arquivo: $arquivo"
+                for outra_palavra in "${outras_palavras[@]}"; do
+                  if grep -q -i "$outra_palavra" "$arquivo"; then
+                    echo "Encontrada a palavra '$outra_palavra' no mesmo arquivo: $arquivo"
+                    encontrada=true
+                    saida="$saida$palavra_chave $outra_palavra no arquivo $arquivo\n"
+                  fi
+                done
+              fi
             done
-          fi
-          
-          echo ">>>>>>>depois do for>>>>>>>>>>>>> $encontrada"
+          done
+        fi
 
-          if [ "$encontrada" == true ]; then
-            echo "::error::Encontrado grant to public"
-            SLACK_MESSAGE="Grant to public no PR: ${{ github.event.number }} - github.com/${GITHUB_REPOSITORY}/pull/${{ github.event.number }}/files (${{ github.base_ref }})\n$saida"
-            curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$SLACK_MESSAGE"'"}' ${{ secrets.SLACK_WEBHOOK_HML_DATABASE }}
-            PR_NUMBER=$(echo "${{ github.event.pull_request.html_url }}" | awk -F'/' '{print $NF}')
-            REPO_OWNER=$(echo "${{ github.repository }}" | cut -d '/' -f 1)
-            REPO_NAME=$(echo "${{ github.repository }}" | cut -d '/' -f 2)
-            AUTHOR_NAME=${{ github.event.pull_request.user.login }}
-            gh pr comment $PR_NUMBER -R $REPO_OWNER/$REPO_NAME --body "@$AUTHOR_NAME
-            ${saida}
-            Por favor revisar."
-            exit 1
-          else
-            echo "PR OK"
-          fi
+        echo ">>>>>>>depois do for>>>>>>>>>>>>> $encontrada"
+
+        if [ "$encontrada" == true ]; then
+          echo "::error::Encontrado grant to public"
+          SLACK_MESSAGE="Grant to public no PR: ${{ github.event.number }} - github.com/${GITHUB_REPOSITORY}/pull/${{ github.event.number }}/files (${{ github.base_ref }})\n$saida"
+          curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$SLACK_MESSAGE"'"}' ${{ secrets.SLACK_WEBHOOK_HML_DATABASE }}
+          PR_NUMBER=$(echo "${{ github.event.pull_request.html_url }}" | awk -F'/' '{print $NF}')
+          REPO_OWNER=$(echo "${{ github.repository }}" | cut -d '/' -f 1)
+          REPO_NAME=$(echo "${{ github.repository }}" | cut -d '/' -f 2)
+          AUTHOR_NAME=${{ github.event.pull_request.user.login }}
+          gh pr comment $PR_NUMBER -R $REPO_OWNER/$REPO_NAME --body "@$AUTHOR_NAME
+          ${saida}
+          Por favor revisar."
+          exit 1
+        else
+          echo "PR OK"
+        fi
