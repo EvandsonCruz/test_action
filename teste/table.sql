d9b443bcf7056912a452cf90a189e1eb6a70c17a
Evandson Cruz <37565222+EvandsonCruz@users.noreply.github.com>
2024-09-03 14:25:34 -0300

    Update table.sql

--- teste/table.sql
+++ teste/table.sql
@@ -9,7 +9,7 @@ CREATE TABLE employees (
     salary NUMBER(8,2),
     commission_pct NUMBER(2,2),
     manager_id NUMBER(6),
-    manager_id date,
+    manager_id NUMBER(6),
     department_id NUMBER(4),
     CONSTRAINT fk_department
         FOREIGN KEY (department_id) REFERENCES departments(department_id)
